423d3e6997987975f3833e7348f9a024903b16c1
bernardopacini <bernardopacini@users.noreply.github.com>
2022-06-02 21:55:37 +0000

    jekyll build from Action a646ae6ed1b85d9b02b00ede7511d2444ded9b50

---
+++ assets/js/nav.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#nav-toggle').click(function() {
+        $('nav ul').slideToggle();
+      });
+      // Hamburger to X toggle
+      $('#nav-toggle').on('click', function() {
+        this.classList.toggle('active');
+      });
+  });
